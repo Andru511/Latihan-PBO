64278274f5d07491f41ae5f0643a05e641de14e3
Andru511 <Shabriri511@gmail.com>
2025-04-17 01:58:19 +0700

    Add files via upload

---
+++ Latihan Peretemuan 6/Birthday/BirthdayGUI.java
@@ -0,0 +1,32 @@
+package Birthday;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BirthdayGUI extends JFrame{
+    public JTextField nameField = new JTextField(15);
+    public JTextField dateField = new JTextField(10);
+    public JButton calculateButton = new JButton("Hitung");
+    public JTextArea resultArea = new JTextArea(5, 30);
+    
+    public BirthdayGUI(){
+        setTitle("penghitung usia");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel ("nama: "));
+        inputPanel.add(nameField);
+        inputPanel.add(new JLabel ("tanggal: "));
+        inputPanel.add(dateField);
+        inputPanel.add(calculateButton);
+        
+        resultArea.setEditable(false);
+        add(inputPanel, BorderLayout.NORTH);
+        add(new JScrollPane(resultArea), BorderLayout.CENTER);
+        
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
---
+++ Latihan Peretemuan 6/Birthday/Mavenproject2.java
@@ -0,0 +1,8 @@
+package Birthday;
+
+public class Mavenproject2 {
+    public static void main(String[] args) {
+       BirthdayGUI gui = new BirthdayGUI();
+       new BirthdayController(gui);
+    }
+}
